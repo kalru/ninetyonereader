0ccf0a760b7e09cb73bdccc7c656823099a703bf
kalru <kalvynroux@yahoo.com>
2023-11-07 23:46:23 +0200

    cleanup and add login

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 
@@ -9,47 +8,28 @@ import (
 	"github.com/playwright-community/playwright-go"
 )
 
+func assertErrorToNilf(message string, err error) {
+	if err != nil {
+		log.Fatalf(message, err)
+	}
+}
+
 func main() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatalf("Could not load .env: %s", err)
-	}
-	val := os.Getenv("TEST")
-	fmt.Println(val)
-	err = playwright.Install()
-	if err != nil {
-		log.Fatalf("could not download and install drivers for playwright: %v", err)
-	}
+	assertErrorToNilf("Could not load .env: %s", godotenv.Load(".env"))
+	assertErrorToNilf("could not download and install drivers for playwright: %v", playwright.Install())
 	pw, err := playwright.Run()
-	if err != nil {
-		log.Fatalf("could not start playwright: %v", err)
-	}
+	assertErrorToNilf("could not start playwright: %v", err)
 	browser, err := pw.Firefox.Launch(playwright.BrowserTypeLaunchOptions{Headless: playwright.Bool(false)})
-	if err != nil {
-		log.Fatalf("could not launch browser: %v", err)
-	}
+	assertErrorToNilf("could not launch browser: %v", err)
 	page, err := browser.NewPage()
-	if err != nil {
-		log.Fatalf("could not create page: %v", err)
-	}
-	if _, err = page.Goto("https://news.ycombinator.com"); err != nil {
-		log.Fatalf("could not goto: %v", err)
-	}
-	entries, err := page.Locator(".athing").All()
-	if err != nil {
-		log.Fatalf("could not get entries: %v", err)
-	}
-	for i, entry := range entries {
-		title, err := entry.Locator("td.title > span > a").TextContent()
-		if err != nil {
-			log.Fatalf("could not get text content: %v", err)
-		}
-		fmt.Printf("%d: %s\n", i+1, title)
-	}
-	if err = browser.Close(); err != nil {
-		log.Fatalf("could not close browser: %v", err)
-	}
-	if err = pw.Stop(); err != nil {
-		log.Fatalf("could not stop Playwright: %v", err)
-	}
+	assertErrorToNilf("could not create page: %v", err)
+	_, err = page.Goto("https://login.secure.ninetyone.com")
+	assertErrorToNilf("could not goto: %v", err)
+	assertErrorToNilf("could not fill in username: %v", page.GetByLabel("Email").Fill(os.Getenv("EMAIL")))
+	assertErrorToNilf("could not fill in password: %v", page.GetByLabel("Password").Fill(os.Getenv("PASSWORD")))
+	assertErrorToNilf("could not log in: %v", page.GetByRole("button", playwright.PageGetByRoleOptions{Name: "LOG IN"}).Click())
+	assertErrorToNilf("could not fill in 2FA code: %v", page.GetByPlaceholder("Enter the 6-digit code").Fill(os.Getenv("2FA_CODE")))
+	assertErrorToNilf("could not log in after 2FA: %v", page.GetByRole("button", playwright.PageGetByRoleOptions{Name: ""}).Click())
+	assertErrorToNilf("could not close browser: %w", browser.Close())
+	assertErrorToNilf("could not stop Playwright: %w", pw.Stop())
 }
